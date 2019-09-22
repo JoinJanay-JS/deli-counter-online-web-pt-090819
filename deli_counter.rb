dc043a0952e75510247ce8f5d10100ed8d083de1
Janay Bailey <tjanay87@gmail.com>
2019-09-22 12:14:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   newArray = []
   puts "There is nobody in line"
     if array.length > 1 
-    counter = 1 
+    counter += 1 
     names.each do |name|
       puts "Welcome, #{name}/ You are number #{counter} in line."
     end 
