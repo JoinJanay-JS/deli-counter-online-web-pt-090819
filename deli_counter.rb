cf18ba66f3732218b5989a6d28403c94066ff117
Janay Bailey <tjanay87@gmail.com>
2019-09-22 12:08:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,12 @@ katz_deli = []
 def line(array)
   if array.length = 0 
     counter = 0 
-    puts "There is nobody in "
-  if array.length > 1 
+    puts "There is nobody in line."
+  elsif array.length > 1 
     counter = 1 
     names.each do |name|
       puts "Welcome, #{name}/ You are number #{counter} in line."
     end 
   end 
+end
 end
