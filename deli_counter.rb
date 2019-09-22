298e02bb63a51c1d151385997a0487d63ed06aed
Janay Bailey <tjanay87@gmail.com>
2019-09-22 12:40:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,4 @@
-names = ["Ada", "Grace", "Kent", "Matz"]
-  
-def line(array)
-  if names.length >= 1
-    katz_deli = []
-    counter = 0 
-    names.each do |name|
-      katz_deli.push ("#{counter}. #{name}")
-      counter += 1
-    end
-      puts "This line is currently: #{katz_deli.join(" ")}"
-      end 
-  puts "This line is currently empty"
-end 
+
 
 line(katz_deli)
 
