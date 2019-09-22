3b218420d8cd627ceb2f1e82c8c53425ead89828
Janay Bailey <tjanay87@gmail.com>
2019-09-22 12:05:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+names = ["Ada", "Grace", "Kent", "Matz"]
+katz_deli = []
+  
+def line(array)
+  if array.length > 1 
+    counter = 1 
+    names.each do |name|
+      puts "Welcome, #{name}/ You are number #{counter} in line."
+    end 
+  end 
+end
