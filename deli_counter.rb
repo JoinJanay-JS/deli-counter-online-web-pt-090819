51dead2f1aff86d9ee699ee0e32500d78956b9aa
Janay Bailey <tjanay87@gmail.com>
2019-09-22 12:41:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,16 @@
-
+def line(array) # this was the one I figured out
+  if array.length >= 1
+    nuarray = []
+    counter = 1 
+    array.each do |name|
+      nuarray.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{nuarray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
 def line_simple(array) # this one follows the same mechanics as learn.co
   current_line = "The simple line is currently:"
   array.each.with_index(1) do |value, indexemus|  
